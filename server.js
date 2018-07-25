341656e97037e75fb264ee93674b1891bd05d34c
william <willshinji@gmail.com>
2018-07-25 13:13:38 -0300

    sets view engine

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
